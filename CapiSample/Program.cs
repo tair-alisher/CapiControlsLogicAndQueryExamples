bff014fb20d1e18419a6d37ba8bb6bd7b67f3066
tair-alisher <ur.alisher@mail.ru>
2019-07-04 16:35:13 +0600

    изменена логика выбора контроля, теперь сначала отображается список доступных для проверки форм, после выбора формы, отображается список доступных контролей; номер контроля составляется из номера формы и номера контроля.

--- CapiSample/Program.cs
+++ CapiSample/Program.cs
@@ -17,119 +17,142 @@ namespace CapiSample
                 bool done = false;
                 Directory.CreateDirectory("Reports");
 
-                Console.WriteLine("Доступные контроли:");
-                Console.WriteLine("1: Форма 3. Раздел 1. Единицы измерения.");
-                Console.WriteLine("2: Форма 3. Разедл 2. Единицы измерения.");
-                Console.WriteLine("3: Форма 3. Раздел 2. Источники поступления.");
-                Console.WriteLine("4: Форма 5. Единицы измерения.");
-                Console.WriteLine("5: Форма 5. Материал.");
-                Console.WriteLine("6: Форма 5. Для кого куплено.");
-                Console.WriteLine("7: Форма 6. Раздел 2. Пункт 1. Расходы на топливо."); // всегда успешно. создан для примера запроса.
-                Console.WriteLine("8: Форма 6. Раздел 2. Пункт 1. Стоимость купленного топлива.");
-                Console.WriteLine("9: Форма 6. Раздел 2. Оплата за аренду жилья.");
-                Console.WriteLine("10: Форма 6. Раздел 2. Если есть льготы, указать процент.");
-                Console.WriteLine("11. Форма 6. Раздел 3. Обращения за мед. помощью.");
-                Console.WriteLine("12. Форма 6. Раздел 3. Расходы на амбулаторное лечение.");
-                Console.WriteLine("13. Форма 6. Раздел 3. Госпитализация.");
-                Console.WriteLine("14. Форма 6. Раздел 3. Расходы на стационарное лечение.");
-                Console.WriteLine("15. Форма 6. Раздел 3. Расходы на мед. принадлежности, лекарство.");
-                Console.WriteLine("16. Форма 6. Раздел 4. Расходы на транспорт.");
-                Console.WriteLine("17. Форма 6. Раздел 5. Расходы на дошкольное образование.");
-                Console.WriteLine("18. Форма 6. Раздел 5. Расходы на образование.");
-                Console.WriteLine("19. Форма 6. Раздел 6. Покупка недвижимости.");
-                Console.WriteLine("20. Форма 6. Раздел 6. Трата сбережений.");
-                Console.WriteLine("21. Форма 6. Раздел 6. Вложение сбережений.");
-                Console.WriteLine("22. Форма 6. Раздел 6. Брали ли в долг или кредит.");
-                Console.WriteLine("23. Форма 6. Раздел 6. Возврат долга или кредита.");
-                Console.WriteLine("24. Форма 6. Раздел 6. Уплата налога на недвижимость.");
-                Console.WriteLine("25. Форма 6. Раздел 6. Уплата подоходного налога.");
-
-                Console.Write("\nВыберите номер контроля: ");
-
                 var builder = new ConfigurationBuilder()
                     .SetBasePath(Directory.GetCurrentDirectory())
                     .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
                 IConfiguration config = builder.Build();
                 string connectionString = config.GetConnectionString("remote");
 
-                IControl Control = null;
-                string controlName = Console.ReadLine();
+                Console.WriteLine("Доступные для проверки формы:");
+                Console.WriteLine("3. Форма 3");
+                Console.WriteLine("5. Форма 5");
+                Console.WriteLine("6. Форма 6");
 
-                switch (controlName)
+                Console.Write("\nВыберите номер формы: ");
+                string formNumber = Console.ReadLine();
+
+                Console.WriteLine("Доступные контроли:");
+                switch (formNumber)
                 {
-                    case "1":
-                        Control = new FormThreeSectionOneUnits(connectionString);
-                        break;
-                    case "2":
-                        Control = new FormThreeSectionTwoUnits(connectionString);
-                        break;
                     case "3":
-                        Control = new FormThreeSectionTwoSupplySources(connectionString);
-                        break;
-                    case "4":
-                        Control = new FormFiveUnitsControl(connectionString);
+                        Console.WriteLine("1: Форма 3. Раздел 1. Единицы измерения.");
+                        Console.WriteLine("2: Форма 3. Разедл 2. Единицы измерения.");
+                        Console.WriteLine("3: Форма 3. Раздел 2. Источники поступления.");
                         break;
                     case "5":
-                        Control = new FormFiveMaterialsControl(connectionString);
+                        Console.WriteLine("1: Форма 5. Единицы измерения.");
+                        Console.WriteLine("2: Форма 5. Материал.");
+                        Console.WriteLine("3: Форма 5. Для кого куплено.");
                         break;
                     case "6":
+                        Console.WriteLine("1: Форма 6. Раздел 2. Пункт 1. Расходы на топливо.");
+                        Console.WriteLine("2: Форма 6. Раздел 2. Пункт 1. Стоимость купленного топлива.");
+                        Console.WriteLine("3: Форма 6. Раздел 2. Оплата за аренду жилья.");
+                        Console.WriteLine("4: Форма 6. Раздел 2. Если есть льготы, указать процент.");
+                        Console.WriteLine("5. Форма 6. Раздел 3. Обращения за мед. помощью.");
+                        Console.WriteLine("6. Форма 6. Раздел 3. Расходы на амбулаторное лечение.");
+                        Console.WriteLine("7. Форма 6. Раздел 3. Госпитализация.");
+                        Console.WriteLine("8. Форма 6. Раздел 3. Расходы на стационарное лечение.");
+                        Console.WriteLine("9. Форма 6. Раздел 3. Расходы на мед. принадлежности, лекарство.");
+                        Console.WriteLine("10. Форма 6. Раздел 4. Расходы на транспорт.");
+                        Console.WriteLine("11. Форма 6. Раздел 5. Расходы на дошкольное образование.");
+                        Console.WriteLine("12. Форма 6. Раздел 5. Расходы на образование.");
+                        Console.WriteLine("13. Форма 6. Раздел 6. Покупка недвижимости.");
+                        Console.WriteLine("14. Форма 6. Раздел 6. Трата сбережений.");
+                        Console.WriteLine("15. Форма 6. Раздел 6. Вложение сбережений.");
+                        Console.WriteLine("16. Форма 6. Раздел 6. Брали ли в долг или кредит.");
+                        Console.WriteLine("17. Форма 6. Раздел 6. Возврат долга или кредита.");
+                        Console.WriteLine("18. Форма 6. Раздел 6. Уплата налога на недвижимость.");
+                        Console.WriteLine("19. Форма 6. Раздел 6. Уплата подоходного налога.");
+                        break;
+                    default:
+                        done = true;
+                        break;
+                }
+
+                if (done)
+                    break;
+
+                Console.Write("\nВыберите номер контроля: ");
+
+                IControl Control = null;
+                string controlCollectedNumber = formNumber + Console.ReadLine();
+
+                switch (controlCollectedNumber)
+                {
+                    case "31":
+                        Control = new FormThreeSectionOneUnits(connectionString);
+                        break;
+                    case "32":
+                        Control = new FormThreeSectionTwoUnits(connectionString);
+                        break;
+                    case "33":
+                        Control = new FormThreeSectionTwoSupplySources(connectionString);
+                        break;
+                    case "51":
+                        Control = new FormFiveUnitsControl(connectionString);
+                        break;
+                    case "52":
+                        Control = new FormFiveMaterialsControl(connectionString);
+                        break;
+                    case "53":
                         Control = new FormFiveItemUsersControl(connectionString);
                         break;
-                    case "7":
+                    case "61":
                         Control = new FormSixSectionTwoQuestionOneOccupancy(connectionString);
                         break;
-                    case "8":
+                    case "62":
                         Control = new FormSixSectionTwoQuestionOneCost(connectionString);
                         break;
-                    case "9":
+                    case "63":
                         Control = new FormSixSectionTwoRentPaymentQuestion(connectionString);
                         break;
-                    case "10":
+                    case "64":
                         Control = new FormSixSectionTwoServices(connectionString);
                         break;
-                    case "11":
+                    case "65":
                         Control = new FormSixSectionThreeTreatment(connectionString);
                         break;
-                    case "12":
+                    case "66":
                         Control = new FormSixSectionThreeTreatmentCost(connectionString);
                         break;
-                    case "13":
+                    case "67":
                         Control = new FormSixSectionThreeHospitalization(connectionString);
                         break;
-                    case "14":
+                    case "68":
                         Control = new FormSixSectionThreeHospitalizationCost(connectionString);
                         break;
-                    case "15":
+                    case "69":
                         Control = new FormSixSectionThreeMedicine(connectionString);
                         break;
-                    case "16":
+                    case "610":
                         Control = new FormSixSectionFour(connectionString);
                         break;
-                    case "17":
+                    case "611":
                         Control = new FormSixSectionFivePreSchoolEduCost(connectionString);
                         break;
-                    case "18":
+                    case "612":
                         Control = new FormSixSectionFiveEducation(connectionString);
                         break;
-                    case "19":
+                    case "613":
                         Control = new FormSixSectionSixPropertyPurchase(connectionString);
                         break;
-                    case "20":
+                    case "614":
                         Control = new FormSixSectionSixSavingsWaste(connectionString);
                         break;
-                    case "21":
+                    case "615":
                         Control = new FormSixSectionSixSavingsContribution(connectionString);
                         break;
-                    case "22":
+                    case "616":
                         Control = new FormSixSectionSixLoanGetting(connectionString);
                         break;
-                    case "23":
+                    case "617":
                         Control = new FormSixSectionSixLoanRepayment(connectionString);
                         break;
-                    case "24":
+                    case "618":
                         Control = new FormSixSectionSixPropertyTax(connectionString);
                         break;
-                    case "25":
+                    case "619":
                         Control = new FormSixSectionSixIncomeTax(connectionString);
                         break;
                     default:
