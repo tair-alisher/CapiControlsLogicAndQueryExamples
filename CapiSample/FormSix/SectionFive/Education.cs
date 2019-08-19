8f52d29707d36a8e0736572cfd7c00858bc40822
tair-alisher <ur.alisher@mail.ru>
2019-08-19 11:19:26 +0600

    изменены контроли пятого раздела шестой формы; теперь проверка валидности ответа проверяется в запросе, результатом запроса является список данных о невалидных ответах.

--- CapiSample/FormSix/SectionFive/Education.cs
+++ CapiSample/FormSix/SectionFive/Education.cs
@@ -5,65 +5,39 @@ using System.IO;
 
 namespace CapiSample.FormSix.SectionFive
 {
-    internal class Education : BaseControl<AnswerDataWithValidRow>, IControl
+    internal class Education : BaseControl<AnswerData>, IControl
     {
         public Education(string connection) : base(connection) { }
 
         public void Execute()
         {
             CheckAnswers(base.CreateFile());
-            Console.WriteLine("Расходы на образование. Проверено.");
+            Console.WriteLine("Раздел 5. Вопрос 4. Сколько вы потратили на учебу. Проверено.");
         }
 
         private void CheckAnswers(FileStream file)
         {
-            var answers = base.ExecuteQuery(educationExpensesMustGreaterThanZeroQuery);
+            var wrongAnswers = base.ExecuteQuery(answersWithInvalidValuesQuery);
             using (var writer = File.AppendText(file.Name))
             {
-                foreach (var answer in answers)
-                {
-                    if (!answer.ValidRow)
-                        writer.WriteLine($"interview: {answer.InterviewKey}; Расходы на школьное образование за один или более месяцев должны быть больше нуля.");
-                }
+                foreach (var wrongAnswer in wrongAnswers)
+                    base.WriteError(writer, wrongAnswer);
             }
             file.Close();
         }
 
-        private readonly string educationExpensesMustGreaterThanZeroQuery = @"select summary.summaryid as InterviewId
+        private readonly string answersWithInvalidValuesQuery = @"select
+    summary.summaryid as InterviewId
+    ,'Форма 6' as Form
+    ,'Раздел 5' as Section
+    ,'Вопрос 4' as QuestionNumber
+    ,'Сколько Вы потратили на учебу?' as QuestionText
+    ,'Должна быть указана сумма расходов' as InfoMessage
     ,summary.key as InterviewKey
     ,summary.questionnairetitle as QuestionnaireTitle
     ,summary.updatedate as InterviewDate
     ,summary.teamleadname as Region
     ,qe.stata_export_caption as QuestionCode
-    ,(coalesce(interview.asint, 0)
-      + coalesce(
-        (
-            select _interview.asint
-            from readside.interviews as _interview
-                join readside.questionnaire_entities as _qe
-                    on _interview.entityid = _qe.id
-                join readside.interviews_id as _interview_id
-                    on _interview.interviewid = _interview_id.id
-            where _qe.stata_export_caption = 'f6r5q41A3'
-                and _qe.parentid = qe.parentid
-                and _interview.rostervector = interview.rostervector
-                and _interview.interviewid = interview.interviewid
-            limit 1
-        ), 0)
-    + coalesce(
-        (
-            select _interview.asint
-            from readside.interviews as _interview
-                join readside.questionnaire_entities as _qe
-                    on _interview.entityid = _qe.id
-                join readside.interviews_id as _interview_id
-                    on _interview.interviewid = _interview_id.id
-            where _qe.stata_export_caption = 'f6r5q41A4'
-                and _qe.parentid = qe.parentid
-                and _interview.rostervector = interview.rostervector
-                and _interview.interviewid = interview.interviewid
-            limit 1
-        ), 0) > 0) as ValidRow
 from readside.interviews as interview
     join readside.questionnaire_entities as qe
         on interview.entityid = qe.id
@@ -82,7 +56,33 @@ where qe.stata_export_caption = 'f6r5q41A2'
           where _qe.stata_export_caption = 'f6r5q3'
               and _interview_id.interviewid = interview_id.interviewid
           limit 1
-      ) = 1 
+      ) = 1
+      and (coalesce(interview.asint, 0)
+      + coalesce(
+        (select _interview.asint
+        from readside.interviews as _interview
+            join readside.questionnaire_entities as _qe
+                on _interview.entityid = _qe.id
+            join readside.interviews_id as _interview_id
+                on _interview.interviewid = _interview_id.id
+        where _qe.stata_export_caption = 'f6r5q41A3'
+            and _qe.parentid = qe.parentid
+            and _interview.rostervector = interview.rostervector
+            and _interview.interviewid = interview.interviewid
+        limit 1), 0)
+    + coalesce(
+        (select _interview.asint
+        from readside.interviews as _interview
+            join readside.questionnaire_entities as _qe
+                on _interview.entityid = _qe.id
+            join readside.interviews_id as _interview_id
+                on _interview.interviewid = _interview_id.id
+        where _qe.stata_export_caption = 'f6r5q41A4'
+            and _qe.parentid = qe.parentid
+            and _interview.rostervector = interview.rostervector
+            and _interview.interviewid = interview.interviewid
+        limit 1), 0)
+    > 0) is false
 order by summary.interviewid";
     }
 }
